ec94c0bc9d70f0d6a0fb3f404a703dc62ce46480
toloza01 <estebantoloza1998@gmail.com>
2020-09-11 22:32:51 -0300

    subiendo diseño

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const grid = new Muuri('.grid',{
+    layout: {
+        rounding: false
+    }
+});
+
+
+
+//Buscador de proyectos
+
+filter = document.querySelector('#input-proyectos');
+console.log(filter.value);
